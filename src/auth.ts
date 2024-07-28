4d3c76fd44f3fe71174c7cf20ddb0d93e4bde72f
Fade <lifelessbotfromdiscord@gmail.com>
2024-07-28 13:37:49 +0530

    remove user model

--- src/auth.ts
+++ src/auth.ts
@@ -1,29 +1,9 @@
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
-import dbConnect from "./lib/dbConnect";
-import User from "./models/user.model";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google({
     clientId: process.env.AUTH_GOOGLE_ID,
     clientSecret: process.env.AUTH_GOOGLE_SECRET,
-  })],
-  callbacks: {
-    signIn: async ({ user }) => {
-      try {
-        const { email, name, image, id } = user;
-        await dbConnect();
-        const alreadyUser = await User.findOne({ email });
-
-        if (!alreadyUser) {
-          await User.create({ email, name, image, authProviderId: id });
-          return true;
-        } else {
-          return true;
-        }
-      } catch (error) {
-        throw new Error("Error while creating user");
-      }
-    },
-  }
+  })]
 })
